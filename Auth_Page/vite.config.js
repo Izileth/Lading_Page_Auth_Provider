8b1682784532b396af4e0ad92f0e9a265b7993d9
Kawã Kleber <kawaklebersc@gmail.com>
2025-03-14 17:07:18 -0300

    Edit Routes

--- Auth_Page/vite.config.js
+++ Auth_Page/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Auth_Page/', // Isso garante que os caminhos estejam corretos
+  base: '/', // Isso garante que os caminhos estejam corretos
 })
