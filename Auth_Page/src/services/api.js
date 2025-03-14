dcd38fb1d0ac1657b33e626237a84cf8dab3eced
Kawã Kleber <kawaklebersc@gmail.com>
2025-03-14 16:50:46 -0300

    Edit Config

--- Auth_Page/src/services/api.js
+++ Auth_Page/src/services/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
+// Substitua pelo seu URL do Render
 const api = axios.create({
-    baseURL: 'http://localhost:3100',
-})
+    baseURL: 'https://api-rest-utopic.onrender.com', // Substitua com o URL correto do Render
+});
 
-export default api
+export default api;
